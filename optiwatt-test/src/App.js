5344cebf8652168c0088fdcd009b7e330c6e6bf4
Eli Zevin <dizzyjaguar@gmail.com>
2022-04-12 13:09:10 -0700

    last little changes

--- optiwatt-test/src/App.js
+++ optiwatt-test/src/App.js
@@ -6,12 +6,14 @@ import { ReactComponent as Icon1 } from "./assets/icon1.svg";
 function App() {
   return (
     <div className="App">
-      <h3>FEATURES</h3>
-      <h1>Here is more fo what it can do you for you</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit sit mus
-        turpis sed sapien amet, purus condimentum sit.
-      </p>
+      <div className="HeadingContainer">
+        <h3>FEATURES</h3>
+        <h1>Here is more of what it can do you for you</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit sit mus
+          turpis sed sapien amet, purus condimentum sit.
+        </p>
+      </div>
       <div className="FeatureGrid">
         <Feature title={"Feature 1"} icon={<Icon1 />} />
         <Feature title={"Feature 1"} icon={<Icon1 />} />
